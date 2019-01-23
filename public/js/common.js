b82ced4bc4a2ebb86c6fbf999e814cb62e69e525
고재훈 <works.hoon@gmail.com>
2019-01-24 02:13:26 +0900

    0123 작업파일

---
+++ public/js/common.js
@@ -0,0 +1,115 @@
+var likeBtnFn = function(obj){//좋아요버튼
+		var $this = obj,
+			count = $this.attr('data-count')*1,
+			w = $this.width();
+		if($this.hasClass('selected')){
+			$this.removeClass('selected');
+			count--;
+		}else{
+			$this.addClass('selected');
+			count++;
+		}
+		$this.attr('data-count',count).find('.count').text(numberWithCommas(count));
+		if( w!=$this.width() ){
+			brandWidthSet($this.closest('li').find('.func'));
+		}
+		if($this.is('[data-callback]')){
+			var cb = $this.attr('data-callback');
+			window[cb].call($this[0]);
+		}
+	},
+	numberWithCommas = function(x){
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	},
+	resizeFn = function(e){
+		$('.list_product:not(.small) .func').each(function(i,el) {
+			brandWidthSet(el);
+		});
+	},
+	brandWidthSet = function(el){
+		var w = $(el).closest('li').width()-10;
+		$(el).closest('li').find('.brand').css('max-width',w-$(el).width());
+	},
+	qnaViewFn = function(obj){
+		var $this = $(obj);
+		$this.closest('.group').find('.item').removeClass('view');
+		$this.closest('.item').addClass('view');
+	};
+$(document).ready(function(){
+	$(window).on({
+		click:function(e){
+			var $this = $(e.target);
+			if( $this.closest('button').is('[data-type=like]') ){//좋아요버튼
+				likeBtnFn($this.closest('button'));
+			}
+			if( $this.closest('a').hasClass('disabled') ){//a링크 비활성화
+				e.preventDefault();
+			}
+			if( $this.closest('button').is('[data-type=qnaview]') ){//qna 버튼 열기 닫기
+				qnaViewFn($this[0]);
+			}
+			if( $this.closest('button').is('[data-type=inputreset]') ){//input 리셋하기
+				$this.closest('button').prev('input').val('');
+			}
+		},
+		mouseover:function(e){
+			var $this = $(e.target);
+			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+				$this.closest('button').prev().addClass('over')
+			}
+		},
+		mouseout:function(e){
+			var $this = $(e.target);
+			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+				$this.closest('button').prev().removeClass('over')
+			}
+		},
+		focusin:function(e){
+			var $this = $(e.target);
+			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+				$this.closest('button').prev().addClass('over')
+			}
+		},
+		focusout:function(e){
+			var $this = $(e.target);
+			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+				$this.closest('button').prev().removeClass('over')
+			}
+		},
+		keyup:function(e){
+			var $this = $(e.target);
+			if( $this.is('textarea[data-type=countlength]') ){//textarea 글자수 세기
+				var max = $this.attr('maxlength')*1,
+					count = $this.val().length;
+				$this.next('.count').find('span').text(count);
+			}
+		},
+		resize:function(e){
+			resizeFn()
+		},
+		load:function(e){
+			resizeFn()
+		}
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
