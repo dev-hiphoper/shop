6b669b61b9b0b507458d0600653c89035623c1d1
고재훈 <works.hoon@gmail.com>
2019-02-04 00:59:05 +0900

    - 크로스브라우징 작업 완료 - 반응형 수정 작업 완료

--- public/js/common.js
+++ public/js/common.js
@@ -25,10 +25,17 @@ var likeBtnFn = function(obj){//좋아요버튼
 		$('.list_product:not(.small) .func').each(function(i,el) {
 			brandWidthSet(el);
 		});
+		$('.street_list .func').each(function(i,el) {
+			brandWidthSet2(el);
+		});
 	},
 	brandWidthSet = function(el){
 		var w = $(el).closest('li').width()-10;
 		$(el).closest('li').find('.brand').css('max-width',w-$(el).width());
+	},
+	brandWidthSet2 = function(el){
+		var w = $(el).closest('li').width()-25;
+		$(el).closest('li').find('.category').css('max-width',w-$(el).width());
 	},
 	qnaViewFn = function(obj){
 		var $this = $(obj);
@@ -50,95 +57,96 @@ var likeBtnFn = function(obj){//좋아요버튼
 			obj.value = obj.value.slice(0, obj.maxLength);
 		}
 	};
-$(document).ready(function(){
-	$(window).on({
-		click:function(e){
-			var $this = $(e.target);
-			if( $this.closest('button').is('[data-type=like]') ){//좋아요버튼
-				likeBtnFn($this.closest('button'));
-			}
-			if( $this.closest('a').hasClass('disabled') ){//a링크 비활성화
-				e.preventDefault();
-			}
-			if( $this.closest('button').is('[data-type=qnaview]') ){//qna 버튼 열기 닫기
-				qnaViewFn($this[0]);
-			}
-			if( $this.closest('button').is('[data-type=inputreset]') ){//input 리셋하기
-				$this.closest('button').prev('input').val('');
-			}
-			if( $this.closest('a').is('[data-type=thumbnail]') ){//상품 상세 이미지
-				e.preventDefault();
-				$this.closest('a').closest('.thumbnails').find('.active').removeClass('active');
-				$('img[data-type="visual"]').attr('src',$this.closest('a').attr('href'));
-				$this.closest('a').addClass('active');
-			}
-			if( $this.is('[data-type=plus]') ){//input 숫자 더하기
-				setNumber('plus',$this);
-			}
-			if( $this.is('[data-type=minus]') ){//input 숫자 빼기
-				setNumber('minus',$this);
-			}
-		},
-		change:function(e){
-			var $this = $(e.target);
-			if( $this.is('select.tc_3_h') ){//셀렉트 박스 컬러 변경
-				$this.addClass('tc_3');
-			}
-			if( $this.is('input[data-type=checkAll]') ){//테이블 체크박스 전체 선택
-				var checkboxs = $this.closest('table').find('tbody input[type=checkbox]:not(:disabled)');
-				checkboxs.each(function(e){
-					$(this).prop('checked',$this.prop('checked'))
-				})
-			}
-		},
-		mouseover:function(e){
-			var $this = $(e.target);
-			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
-				$this.closest('button').prev().addClass('over')
-			}
-		},
-		mouseout:function(e){
-			var $this = $(e.target);
-			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
-				$this.closest('button').prev().removeClass('over')
-			}
-		},
-		focusin:function(e){
-			var $this = $(e.target);
-			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
-				$this.closest('button').prev().addClass('over')
-			}
-		},
-		focusout:function(e){
-			var $this = $(e.target);
-			if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
-				$this.closest('button').prev().removeClass('over')
-			}
-		},
-		keyup:function(e){
-			var $this = $(e.target);
-			if( $this.is('textarea[data-type=countlength]') ){//textarea 글자수 세기
-				var max = $this.attr('maxlength')*1,
-					count = $this.val().length;
-				$this.next('.count').find('span').text(count);
-			}
-			if( $this.is('[type=number][maxlength]') ){//number maxlength 제한
-				numberMaxlength($this[0]);
-			}
-		},
-		keydown:function(e){
-			var $this = $(e.target);
-			if( $this.is('[type=number][maxlength]') ){//number maxlength 제한
-				numberMaxlength($this[0]);
-			}
-		},
-		resize:function(e){
-			resizeFn()
-		},
-		load:function(e){
-			resizeFn()
+$(window).on({
+	click:function(e){
+		var $this = $(e.target);
+		if( $this.closest('button').is('[data-type=like]') ){//좋아요버튼
+			likeBtnFn($this.closest('button'));
 		}
-	})
+		if( $this.closest('a').hasClass('disabled') ){//a링크 비활성화
+			e.preventDefault();
+		}
+		if( $this.closest('button').is('[data-type=qnaview]') ){//qna 버튼 열기 닫기
+			qnaViewFn($this[0]);
+		}
+		if( $this.closest('button').is('[data-type=inputreset]') ){//input 리셋하기
+			$this.closest('button').prev('input').val('');
+		}
+		if( $this.closest('a').is('[data-type=thumbnail]') ){//상품 상세 이미지
+			e.preventDefault();
+			$this.closest('a').closest('.thumbnails').find('.active').removeClass('active');
+			$('img[data-type="visual"]').attr('src',$this.closest('a').attr('href'));
+			$this.closest('a').addClass('active');
+		}
+		if( $this.is('[data-type=plus]') ){//input 숫자 더하기
+			setNumber('plus',$this);
+		}
+		if( $this.is('[data-type=minus]') ){//input 숫자 빼기
+			setNumber('minus',$this);
+		}
+	},
+	change:function(e){
+		var $this = $(e.target);
+		if( $this.is('select.tc_3_h') ){//셀렉트 박스 컬러 변경
+			$this.addClass('tc_3');
+		}
+		if( $this.is('input[data-type=checkAll]') ){//테이블 체크박스 전체 선택
+			var checkboxs = $this.closest('table').find('tbody input[type=checkbox]:not(:disabled)');
+			checkboxs.each(function(e){
+				$(this).prop('checked',$this.prop('checked'))
+			})
+		}
+	},
+	mouseover:function(e){
+		var $this = $(e.target);
+		if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+			$this.closest('button').prev().addClass('over')
+		}
+	},
+	mouseout:function(e){
+		var $this = $(e.target);
+		if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+			$this.closest('button').prev().removeClass('over')
+		}
+	},
+	focusin:function(e){
+		var $this = $(e.target);
+		if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+			$this.closest('button').prev().addClass('over')
+		}
+	},
+	focusout:function(e){
+		var $this = $(e.target);
+		if( $this.closest('button').is('[data-type*=qnaview]') ){//qna 버튼 오버 줄표시
+			$this.closest('button').prev().removeClass('over')
+		}
+	},
+	keyup:function(e){
+		var $this = $(e.target);
+		if( $this.is('textarea[data-type=countlength]') ){//textarea 글자수 세기
+			var max = $this.attr('maxlength')*1,
+				count = $this.val().length;
+			$this.next('.count').find('span').text(count);
+		}
+		if( $this.is('[type=number][maxlength]') ){//number maxlength 제한
+			numberMaxlength($this[0]);
+		}
+	},
+	keydown:function(e){
+		var $this = $(e.target);
+		if( $this.is('[type=number][maxlength]') ){//number maxlength 제한
+			numberMaxlength($this[0]);
+		}
+	},
+	resize:function(e){
+		resizeFn()
+	},
+	load:function(e){
+		resizeFn()
+	}
+})
+$(document).ready(function(){
+	if($.browser.msie){$('html').addClass('ie')}
 });
 
 
